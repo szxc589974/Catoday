21854dc9685eb6ac774fb2c50b9c5fe08471ec7b
szxc589974 <szxc589974@gmail.com>
2020-12-21 14:27:07 +0800

    add config

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # 載入需要的模組
+import configparser
+
 from __future__ import unicode_literals
 import os
 from flask import Flask, request, abort
@@ -10,8 +12,11 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 app = Flask(__name__)
 
 # LINE 聊天機器人的基本資料
-line_bot_api = LineBotApi('onuCCvT4ps0AgZTtjpvqTWkPZMj0j4watDwDOAjhRmREPADoakKvtSx0ycjyeuATh08cxvIf+QsnlDjYJjBb2jGqWwZUBuGy2J76Pe3Wk/RlominSvkxIyFsdOHAOTKVv9+UTP2FxA3i4XbpOKjmLQdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('0514d217b27b5e876c1e1a4b4623b7ea')
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
+handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
 
 # 接收 LINE 的資訊
 @app.route("/callback", methods=['POST'])
@@ -29,7 +34,7 @@ def callback():
     return 'OK'
 
 # 學你說話
-@handler.add(MessageEvent, message=TextMessage)
+@handler.add(MessageEvent, message=TextMessage) #如果收到文字訊息就執行下面程式碼
 def echo(event):
     
     if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
@@ -40,3 +45,7 @@ def echo(event):
 
 if __name__ == "__main__":
     app.run()
+
+
+#MessageEvent (信息事件)、FollowEvent (加好友事件)、UnfollowEvent (刪好友事件)、JoinEvent (加入聊天室事件)、LeaveEvent (離開聊天室事件)、MemberJoinedEvent (加入群組事件)、MemberLeftEvent (離開群組事件)
+#MessageEvent又依照信息內容再分成TextMessage、ImageMessage、VideoMessage、StickerMessage、FileMessage等等

f16b81e39d4a7a4d8b3e4451ddf7bb033e9f13ee
szxc589974 <szxc589974@gmail.com>
2020-12-22 13:34:12 +0800

    add 寵物醫院

--- main.py
+++ main.py
@@ -69,6 +69,23 @@ def convertAge(event):
         elif type(event.message.text) :
             covert = age[int(event.message.text)-1]
             line_bot_api.reply_message(event.reply_token,TextSendMessage(text=covert))
+        elif event.message.text == "寵物醫院": # 當使用者意圖為詢問午餐時
+            # 建立一個 button 的 template
+            buttons_template_message = TemplateSendMessage(
+                alt_text="Please tell me where you are",
+                template=ButtonsTemplate(
+                    text="Please tell me where you are",
+                    actions=[
+                        URITemplateAction(
+                            label="Send my location",
+                            uri="line://nv/location"
+                        )
+                    ]
+                )
+            )
+        line_bot_api.reply_message(
+            event.reply_token,
+            buttons_template_message)
 
 if __name__ == "__main__":
     app.run()
@@ -76,3 +93,54 @@ if __name__ == "__main__":
 
 #MessageEvent (信息事件)、FollowEvent (加好友事件)、UnfollowEvent (刪好友事件)、JoinEvent (加入聊天室事件)、LeaveEvent (離開聊天室事件)、MemberJoinedEvent (加入群組事件)、MemberLeftEvent (離開群組事件)
 #MessageEvent又依照信息內容再分成TextMessage、ImageMessage、VideoMessage、StickerMessage、FileMessage等等
+
+
+
+"""
+@handler.add(MessageEvent, message=LocationMessage)
+def handle_location_message(event):
+    # 獲取使用者的經緯度
+    lat = event.message.latitude
+    long = event.message.longitude
+
+    # 使用 Google API Start =========
+    # 1. 搜尋附近餐廳
+    nearby_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key={}&location={},{}&rankby=distance&type=restaurant&language=zh-TW".format(GOOGLE_API_KEY, lat, long)
+    nearby_results = requests.get(nearby_url)
+    # 2. 得到最近的20間餐廳
+    nearby_restaurants_dict = nearby_results.json()
+    top20_restaurants = nearby_restaurants_dict["results"]
+    ## CUSTOMe choose rate >= 4
+    res_num = (len(top20_restaurants)) ##20
+    above4=[]
+    for i in range(res_num):
+        try:
+            if top20_restaurants[i]['rating'] > 3.9:
+                #print('rate: ', top20_restaurants[i]['rating'])
+                above4.append(i)
+        except:
+            KeyError
+    if len(above4) < 0:
+        print('no 4 start resturant found')
+    # 3. 隨機選擇一間餐廳
+        restaurant = random.choice(top20_restaurants)
+    restaurant = top20_restaurants[random.choice(above4)]
+    # 4. 檢查餐廳有沒有照片，有的話會顯示
+    if restaurant.get("photos") is None:
+        thumbnail_image_url = None
+    else:
+        # 根據文件，最多只會有一張照片
+        photo_reference = restaurant["photos"][0]["photo_reference"]
+        thumbnail_image_url = "https://maps.googleapis.com/maps/api/place/photo?key={}&photoreference={}&maxwidth=1024".format(GOOGLE_API_KEY, photo_reference)
+    # 5. 組裝餐廳詳細資訊
+    rating = "無" if restaurant.get("rating") is None else restaurant["rating"]
+    address = "沒有資料" if restaurant.get("vicinity") is None else restaurant["vicinity"]
+    details = "南瓜評分：{}\n南瓜地址：{}".format(rating, address)
+
+    # 6. 取得餐廳的 Google map 網址
+    map_url = "https://www.google.com/maps/search/?api=1&query={lat},{long}&query_place_id={place_id}".format(
+        lat=restaurant["geometry"]["location"]["lat"],
+        long=restaurant["geometry"]["location"]["lng"],
+        place_id=restaurant["place_id"]
+    )
+"""

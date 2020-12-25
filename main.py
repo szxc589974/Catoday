a21f1b166da3df9aa80721958d1c6905243d03aa
szxc589974 <szxc589974@gmail.com>
2020-12-25 15:21:09 +0800

    reset Google API key

--- main.py
+++ main.py
@@ -21,8 +21,7 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-#
-#
+
 age = [15,24]
 for i in range(1,19):
     age.append(age[i]+4)
@@ -30,7 +29,7 @@ for i in range(1,19):
 # LINE 聊天機器人的基本資料
 line_bot_api = LineBotApi('onuCCvT4ps0AgZTtjpvqTWkPZMj0j4watDwDOAjhRmREPADoakKvtSx0ycjyeuATh08cxvIf+QsnlDjYJjBb2jGqWwZUBuGy2J76Pe3Wk/RlominSvkxIyFsdOHAOTKVv9+UTP2FxA3i4XbpOKjmLQdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('0514d217b27b5e876c1e1a4b4623b7ea')
-GOOGLE_API_KEY = os.environ.get('AIzaSyDMA-HQJr05I3DJHo4iNQs39rSOUi5EwMA')
+GOOGLE_API_KEY = 'AIzaSyDMA-HQJr05I3DJHo4iNQs39rSOUi5EwMA'
 
 # 接收 LINE 的資訊
 @app.route("/callback", methods=['POST'])
@@ -89,27 +88,26 @@ def handle_location_message(event):
     long = event.message.longitude
 
     # 使用 Google API Start =========
-    # 1. 搜尋附近餐廳
+    # 1. 搜尋附近寵物醫院
     nearby_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key={}&location={},{}&rankby=distance&type=veterinary_care&language=zh-TW".format(GOOGLE_API_KEY, lat, long)
     nearby_results = requests.get(nearby_url)
-    # 2. 得到最近的20間餐廳
-    nearby_restaurants_dict = nearby_results.json()
-    top20_restaurants = nearby_restaurants_dict["results"]
+    # 2. 得到最近的20間醫院
+    nearby_veterinary_care_dict = nearby_results.json()
+    top20_veterinary_care = nearby_veterinary_care_dict["results"]
     ## CUSTOMe choose rate >= 4
-    res_num = (len(top20_restaurants)) ##20
+    res_num = (len(top20_veterinary_care)) ##20
     above4=[]
     for i in range(res_num):
         try:
-            if top20_restaurants[i]['rating'] > 3.9:
-                #print('rate: ', top20_restaurants[i]['rating'])
+            if top20_veterinary_care[i]['rating'] > 3.9 :
                 above4.append(i)
         except:
             KeyError
     if len(above4) < 0:
         print('no 4 start resturant found')
     # 3. 隨機選擇一間餐廳
-        restaurant = random.choice(top20_restaurants)
-    restaurant = top20_restaurants[random.choice(above4)]
+        restaurant = random.choice(top20_veterinary_care)
+    restaurant = top20_veterinary_care[random.choice(above4)]
     # 4. 檢查餐廳有沒有照片，有的話會顯示
     if restaurant.get("photos") is None:
         thumbnail_image_url = None
@@ -134,13 +132,6 @@ if __name__ == "__main__":
     app.run()
 
 
-#MessageEvent (信息事件)、FollowEvent (加好友事件)、UnfollowEvent (刪好友事件)、JoinEvent (加入聊天室事件)、LeaveEvent (離開聊天室事件)、MemberJoinedEvent (加入群組事件)、MemberLeftEvent (離開群組事件)
-#MessageEvent又依照信息內容再分成TextMessage、ImageMessage、VideoMessage、StickerMessage、FileMessage等等
-
-
-
-
-    
-
-
-"""
+#MessageEvent (信息事件)、FollowEvent (加好友事件)、UnfollowEvent (刪好友事件)、JoinEvent (加入聊天室事件)、
+#LeaveEvent (離開聊天室事件)、MemberJoinedEvent (加入群組事件)、MemberLeftEvent (離開群組事件)
+#MessageEvent又依照信息內容再分成TextMessage、ImageMessage、VideoMessage、StickerMessage、FileMessage等

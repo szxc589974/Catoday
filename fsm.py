d6ef6f4164d56cfa87e6b2dec5dede44b19c81c3
szxc589974 <szxc589974@gmail.com>
2020-12-26 02:29:50 +0800

    add fsm.py 2st

--- fsm.py
+++ fsm.py
@@ -1,3 +1,11 @@
+from __future__ import unicode_literals
+import os
+import json
+import requests
+import random
+import configparser
+import fsm
+from flask import Flask, request, abort
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -10,8 +18,110 @@ from linebot.models import (
     LocationMessage,
     TemplateSendMessage, ButtonsTemplate, URITemplateAction
 )
+
+age = [15,24]
+for i in range(1,19):
+    age.append(age[i]+4)
+
 line_bot_api = LineBotApi('onuCCvT4ps0AgZTtjpvqTWkPZMj0j4watDwDOAjhRmREPADoakKvtSx0ycjyeuATh08cxvIf+QsnlDjYJjBb2jGqWwZUBuGy2J76Pe3Wk/RlominSvkxIyFsdOHAOTKVv9+UTP2FxA3i4XbpOKjmLQdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('0514d217b27b5e876c1e1a4b4623b7ea')
 GOOGLE_API_KEY = 'AIzaSyDMA-HQJr05I3DJHo4iNQs39rSOUi5EwMA'
-def age():
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text="請輸入歲數:"))
+
+#
+def askAge(event):
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text="請輸入貓咪歲數:"))
+
+def convertAge(event):
+    covert = age[int(event.message.text)-1]
+    replyCovertAge = "轉換成人類歲數為" + str(covert) + "歲"
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=replyCovertAge))
+
+def sendPicture(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        ImageSendMessage(
+            original_content_url='https://i.imgur.com/zFmUfzB.jpg', 
+            preview_image_url='https://i.imgur.com/zFmUfzB.jpg'
+        )
+    )
+def sendLocation(event):
+    Button_Template = TemplateSendMessage(
+        alt_text='Please tell me where you are',
+        template=ButtonsTemplate(
+            title='Send Location',
+            text='請告訴我你所在的位置!',
+            actions=[
+                URITemplateAction(
+                    label='Send my location',
+                    uri='line://nv/location'
+                )
+            ]
+        )
+    )
+    line_bot_api.reply_message(event.reply_token,Button_Template)
+
+def receiveLocation(event):
+    # 獲取使用者的經緯度
+    lat = event.message.latitude
+    long = event.message.longitude
+
+    # 使用 Google API Start =========
+    # 1. 搜尋附近寵物醫院
+    nearby_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key={}&location={},{}&rankby=distance&type=veterinary_care&language=zh-TW".format(GOOGLE_API_KEY, lat, long)
+    nearby_results = requests.get(nearby_url)
+    # 2. 得到最近的20間醫院
+    nearby_veterinary_care_dict = nearby_results.json()
+    top20_veterinary_care = nearby_veterinary_care_dict["results"]
+
+        ## CUSTOMe choose rate >= 4
+    veterinary_care_num = (len(top20_veterinary_care)) ##20
+    above4=[]
+    for i in range(veterinary_care_num):
+        try:
+            if top20_veterinary_care[i]['rating'] > 4.2 :
+                above4.append(i)
+        except:
+            KeyError
+
+    if len(above4) < 0:
+        print('no 4 start resturant found')
+        # 3. 隨機選擇一間醫院
+        veterinary_care = random.choice(top20_veterinary_care)
+    veterinary_care = top20_veterinary_care[random.choice(above4)]
+        # 4. 檢查醫院有沒有照片，有的話會顯示
+    print(veterinary_care)
+    if veterinary_care.get("photos") is None:
+        thumbnail_image_url = None
+    else:
+            # 根據文件，最多只會有一張照片
+        photo_reference = veterinary_care["photos"][0]["photo_reference"]
+        thumbnail_image_url = "https://maps.googleapis.com/maps/api/place/photo?key={}&photoreference={}&maxwidth=1024".format(GOOGLE_API_KEY, photo_reference)
+        # 5. 組裝醫院詳細資訊
+    rating = "無" if veterinary_care.get("rating") is None else veterinary_care["rating"]
+    address = "沒有資料" if veterinary_care.get("vicinity") is None else veterinary_care["vicinity"]
+    details = "評分：{}\n地址：{}".format(rating, address)
+
+        # 6. 取得醫院的 Google map 網址
+    map_url = "https://www.google.com/maps/search/?api=1&query={lat},{long}&query_place_id={place_id}".format(
+        lat=veterinary_care["geometry"]["location"]["lat"],
+        long=veterinary_care["geometry"]["location"]["lng"],
+        place_id=veterinary_care["place_id"]
+    )
+        # 使用 Google API End =========
+
+    # 回覆使用 Buttons Template
+    Button_Template = TemplateSendMessage(
+        alt_text=veterinary_care["name"],
+        template=ButtonsTemplate(
+            thumbnail_image_url=thumbnail_image_url,
+            title=veterinary_care["name"],
+            text=details,
+            actions=[
+                URITemplateAction(
+                    label='查看地圖',
+                    uri=map_url
+                )
+            ]
+        )
+    )
+    line_bot_api.reply_message(event.reply_token,Button_Template)
--- main.py
+++ main.py
@@ -23,9 +23,7 @@ from linebot.models import (
 app = Flask(__name__)
 
 
-age = [15,24]
-for i in range(1,19):
-    age.append(age[i]+4)
+
 
 # LINE 聊天機器人的基本資料
 line_bot_api = LineBotApi('onuCCvT4ps0AgZTtjpvqTWkPZMj0j4watDwDOAjhRmREPADoakKvtSx0ycjyeuATh08cxvIf+QsnlDjYJjBb2jGqWwZUBuGy2J76Pe3Wk/RlominSvkxIyFsdOHAOTKVv9+UTP2FxA3i4XbpOKjmLQdB04t89/1O/w1cDnyilFU=')
@@ -46,108 +44,25 @@ def callback():
         abort(400)
 
     return 'OK'
-
+askage = 0
 @handler.add(MessageEvent, message = TextMessage) #如果收到文字訊息就執行下面程式碼
-def convertAge(event):
+def receiveText(event):
 
     if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
         if event.message.text == "年齡":
-            fsm.age()
+            fsm.askAge(event)
+            askage = 1
         elif event.message.text == "圖片": 
-            line_bot_api.reply_message(
-                event.reply_token,
-                ImageSendMessage(
-                    original_content_url='https://i.imgur.com/zFmUfzB.jpg', 
-                    preview_image_url='https://i.imgur.com/zFmUfzB.jpg'
-                )
-            )
+            fsm.sendPicture(event)
         elif event.message.text == "寵物醫院" : # 當使用者意圖為詢問寵物醫院時
-            Button_Template = TemplateSendMessage(
-                alt_text='Please tell me where you are',
-                template=ButtonsTemplate(
-                    title='Send Location',
-                    text='請告訴我你所在的位置!',
-                    actions=[
-                        URITemplateAction(
-                            label='Send my location',
-                            uri='line://nv/location'
-                        )
-                    ]
-                )
-            )
-            line_bot_api.reply_message(event.reply_token,Button_Template)
-
+            fsm.sendLocation(event)
         elif type(event.message.text) :
-            covert = age[int(event.message.text)-1]
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text=covert))
-
+            fsm.convertAge
+            askage = 0
 
 @handler.add(MessageEvent, message = LocationMessage)
 def handle_location_message(event):
-    # 獲取使用者的經緯度
-    lat = event.message.latitude
-    long = event.message.longitude
-
-    # 使用 Google API Start =========
-    # 1. 搜尋附近寵物醫院
-    nearby_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key={}&location={},{}&rankby=distance&type=veterinary_care&language=zh-TW".format(GOOGLE_API_KEY, lat, long)
-    nearby_results = requests.get(nearby_url)
-    # 2. 得到最近的20間醫院
-    nearby_veterinary_care_dict = nearby_results.json()
-    top20_veterinary_care = nearby_veterinary_care_dict["results"]
-
-        ## CUSTOMe choose rate >= 4
-    veterinary_care_num = (len(top20_veterinary_care)) ##20
-    above4=[]
-    for i in range(veterinary_care_num):
-        try:
-            if top20_veterinary_care[i]['rating'] > 4.2 :
-                above4.append(i)
-        except:
-            KeyError
-
-    if len(above4) < 0:
-        print('no 4 start resturant found')
-        # 3. 隨機選擇一間餐廳
-        veterinary_care = random.choice(top20_veterinary_care)
-    veterinary_care = top20_veterinary_care[random.choice(above4)]
-        # 4. 檢查餐廳有沒有照片，有的話會顯示
-    print(veterinary_care)
-    if veterinary_care.get("photos") is None:
-        thumbnail_image_url = None
-    else:
-            # 根據文件，最多只會有一張照片
-        photo_reference = veterinary_care["photos"][0]["photo_reference"]
-        thumbnail_image_url = "https://maps.googleapis.com/maps/api/place/photo?key={}&photoreference={}&maxwidth=1024".format(GOOGLE_API_KEY, photo_reference)
-        # 5. 組裝餐廳詳細資訊
-    rating = "無" if veterinary_care.get("rating") is None else veterinary_care["rating"]
-    address = "沒有資料" if veterinary_care.get("vicinity") is None else veterinary_care["vicinity"]
-    details = "評分：{}\n地址：{}".format(rating, address)
-
-        # 6. 取得餐廳的 Google map 網址
-    map_url = "https://www.google.com/maps/search/?api=1&query={lat},{long}&query_place_id={place_id}".format(
-        lat=veterinary_care["geometry"]["location"]["lat"],
-        long=veterinary_care["geometry"]["location"]["lng"],
-        place_id=veterinary_care["place_id"]
-    )
-        # 使用 Google API End =========
-
-    # 回覆使用 Buttons Template
-    Button_Template = TemplateSendMessage(
-        alt_text=veterinary_care["name"],
-        template=ButtonsTemplate(
-            thumbnail_image_url=thumbnail_image_url,
-            title=veterinary_care["name"],
-            text=details,
-            actions=[
-                URITemplateAction(
-                    label='查看地圖',
-                    uri=map_url
-                )
-            ]
-        )
-    )
-    line_bot_api.reply_message(event.reply_token,Button_Template)
+    fsm.receiveLocation(event)
 
 
                         

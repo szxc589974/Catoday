7c8924905e0a0607104766d44c38681a7031b04e
szxc589974 <szxc589974@gmail.com>
2020-12-27 04:46:50 +0800

    MessageTemplateAction

--- fsm.py
+++ fsm.py
@@ -16,7 +16,7 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
     ImageSendMessage,
-    LocationMessage,
+    LocationMessage,MessageTemplateAction
     TemplateSendMessage, ButtonsTemplate, URITemplateAction
 )
 

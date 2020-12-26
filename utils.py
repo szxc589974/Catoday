1fcefed7c1db2ee74971ebce66c3ed52b1cc90c0
szxc589974 <szxc589974@gmail.com>
2020-12-27 04:28:23 +0800

    delete something

--- utils.py
+++ utils.py
@@ -4,22 +4,19 @@ from linebot import LineBotApi, WebhookParser
 from linebot.models import MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage, ImageCarouselColumn, ImageCarouselTemplate, URITemplateAction, ButtonsTemplate, MessageTemplateAction, ImageSendMessage
 from olami import Olami
 
-channel_access_token = os.getenv("onuCCvT4ps0AgZTtjpvqTWkPZMj0j4watDwDOAjhRmREPADoakKvtSx0ycjyeuATh08cxvIf+QsnlDjYJjBb2jGqWwZUBuGy2J76Pe3Wk/RlominSvkxIyFsdOHAOTKVv9+UTP2FxA3i4XbpOKjmLQdB04t89/1O/w1cDnyilFU=", None)
+line_bot_api = LineBotApi('onuCCvT4ps0AgZTtjpvqTWkPZMj0j4watDwDOAjhRmREPADoakKvtSx0ycjyeuATh08cxvIf+QsnlDjYJjBb2jGqWwZUBuGy2J76Pe3Wk/RlominSvkxIyFsdOHAOTKVv9+UTP2FxA3i4XbpOKjmLQdB04t89/1O/w1cDnyilFU=')
 
 def send_text_message(reply_token, text):
-    line_bot_api = LineBotApi(channel_access_token)
     line_bot_api.reply_message(reply_token, TextSendMessage(text=text))
 
     return "OK"
 
 def send_text_message_AI(reply_token, text):
-    line_bot_api = LineBotApi(channel_access_token)
     line_bot_api.reply_message(reply_token,TextSendMessage(text=Olami().nli(text)))
 
     return "OK"
 
 def send_button_message(reply_token, alt_text, title, text, btn):
-    line_bot_api = LineBotApi(channel_access_token)
     message = TemplateSendMessage(
         alt_text='button template',
         template = ButtonsTemplate(
@@ -33,7 +30,6 @@ def send_button_message(reply_token, alt_text, title, text, btn):
     return "OK"
 
 def send_image_message(reply_token, url):
-    line_bot_api = LineBotApi(channel_access_token)
     message = ImageSendMessage(
         original_content_url = url,
         preview_image_url = url
